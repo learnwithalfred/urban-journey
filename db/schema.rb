2f2509216c108ad65adbe893237f394f3e323573
Alfred Boateng <programmingwithalfred@gmail.com>
2022-11-21 10:55:34 +0000

    Add group

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_094647) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_102905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name", limit: 250, null: false
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
     t.string "icon", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_094647) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users"
 end
