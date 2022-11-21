b226df528ccb36834457e8d154c3cb0036cf4aed
Alfred Boateng <programmingwithalfred@gmail.com>
2022-11-21 09:40:36 +0000

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,19 @@
 class User < ApplicationRecord
-
   MAX_LENGTH = 35
   MIN_LENGTH = 1
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: MAX_LENGTH }
   validates :email,
-    presence: true,
-    uniqueness: { case_sensitive: false },
-    length: { maximum: MAX_LENGTH },
-    format: { with: VALID_EMAIL_REGEX }
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: MAX_LENGTH },
+            format: { with: VALID_EMAIL_REGEX }
   validates :password,
-    presence: true,
-    confirmation: true,
-    length: { minimum: 6 }
+            presence: true,
+            confirmation: true,
+            length: { minimum: 6 }
   validates :password_confirmation, presence: true, on: :create
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -26,5 +25,4 @@ class User < ApplicationRecord
   def to_lowercase
     email.downcase!
   end
-
 end
