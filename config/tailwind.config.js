308454bea5d9b67d0ae5b95d26e90ca12bebbf18
Alfred Boateng <programmingwithalfred@gmail.com>
2022-11-22 08:56:13 +0000

    Update colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,12 +5,17 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primaryColor: '#3778c2',
+        secondaryColor: '#5fb523',
+        tertiaryColor: '#434b54',
       },
     },
   },
@@ -18,5 +23,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-  ]
-}
+  ],
+};
